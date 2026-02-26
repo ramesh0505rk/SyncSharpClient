8a94e46538616e5fcff7153ae4a345f8d40b7bf7
Rameshkumar Krishnasamy <ramesh0505rk@gmail.com>
2026-02-26 10:26:49 +0530

    Initialized signin form

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
 
+  signInForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.signInForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]]
+    })
+  }
+
+  onSubmit() {
+
+  }
 }
