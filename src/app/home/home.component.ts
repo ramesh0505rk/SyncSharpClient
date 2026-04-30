2eb49f4ba4325deaa8d08317bd2ea5a96859f5e6
RAMESHKUMAR KRISHNASAMY <t476797@deluxe.com>
2026-04-30 21:00:28 +0530

    Implemented nav bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  private btns!: NodeListOf<HTMLButtonElement>;
+  private bubble!: HTMLElement | null;
+
+  ngOnInit(): void {
+    this.btns = document.querySelectorAll('.tab-btn');
+    this.bubble = document.getElementById('bubble');
+
+    this.btns.forEach((btn, i) => {
+      btn.addEventListener('click', () => { this.moveBubble(i) });
+    })
+
+    window.addEventListener('load', () => { this.moveBubble(0) });
+    setTimeout(() => this.moveBubble(0), 50);
+  }
+
+  moveBubble(index: number) {
+    const btn = this.btns[index];
+    const container = btn.parentElement;
+    const containerRect = container?.getBoundingClientRect();
+    const btnRect = btn.getBoundingClientRect();
+
+    this.bubble!.style.left = `${btnRect.left - containerRect!.left}px`;
+    this.bubble!.style.width = `${btnRect.width}px`;
+
+    this.btns.forEach((b, i) => {
+      b.style.fontWeight = i === index ? '500' : '400';
+      b.style.color = i === index ? '#FFFFFF' : '#FBFBFB';
+    })
+  }
 }
