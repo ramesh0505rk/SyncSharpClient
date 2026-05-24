5b63a53f09021813351ebea75e2ab4890ff51d13
Rameshkumar Krishnasamy <ramesh0505rk@gmail.com>
2026-05-24 12:25:50 +0530

    Completed work service

--- src/app/Models/work.model.ts
+++ src/app/Models/work.model.ts
@@ -9,6 +9,27 @@ export interface Work {
     LastModified: Date;
 }
 
+export interface CreateWork {
+    title: string;
+    description: string;
+    code: string;
+    language: string;
+    createdBy: string;
+}
+
+export interface UpdateWork {
+    workID: string;
+    code: string;
+    language: string;
+    modifiedBy: string;
+}
+
+export interface User {
+    userID: string;
+    username: string;
+    email: string;
+}
+
 export interface WorkDetail extends Work {
     activeUsers: ActiveUser[];
 }
--- src/app/services/work.service.ts
+++ src/app/services/work.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../Environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Work, WorkDetail } from '../Models/work.model';
+import { Work, WorkDetail, CreateWork, UpdateWork, User } from '../Models/work.model';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +17,41 @@ export class WorkService {
     return this.http.get<Work>(`${this.restApiUrl}/Work/${workID}`);
   }
 
-  getWorkDetail(workID: string) {
+  getWorkDetail(workID: string): Observable<WorkDetail> {
     return this.http.get<WorkDetail>(`${this.restApiUrl}/Work/${workID}/Detail`);
+  }
+
+  getUserWorks(userID: string): Observable<Work[]> {
+    return this.http.get<Work[]>(`${this.restApiUrl}/Work/GetUserWorks/${userID}`);
+  }
+
+  createWork(work: CreateWork): Observable<{ workID: string }> {
+    return this.http.post<{ workID: string }>(`${this.restApiUrl}/Work/CreateWork`, work);
+  }
+
+  updateWork(work: UpdateWork) {
+    return this.http.put(`${this.restApiUrl}/Work/UpdateWork`, work);
+  }
+
+  deleteWork(workID: string) {
+    return this.http.delete(`${this.restApiUrl}/Work/DeleteWork/${workID}`);
+  }
+
+  // Versions (Later)
+  // getWorkVersions(id: number, limit: number = 20): Observable<WorkVersion[]> {
+  //   return this.http.get<WorkVersion[]>(`${this.baseUrl}/${id}/versions?limit=${limit}`);
+  // }
+
+  // Members
+  getMembers(workID: string): Observable<User[]> {
+    return this.http.get<User[]>(`${this.restApiUrl}/Work/${workID}/Members`);
+  }
+
+  addMember(workID: string, userID: string): Observable<any> {
+    return this.http.post(`${this.restApiUrl}/Work/AddWorkMember`, { workID, userID });
+  }
+
+  removeMember(workID: string, userID: string): Observable<any> {
+    return this.http.post(`${this.restApiUrl}/Work/DeleteWorkMember`, { workID, userID });
   }
 }
