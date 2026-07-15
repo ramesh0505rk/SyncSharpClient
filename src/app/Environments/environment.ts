e5d1ba5bcd7e0085474c3ec30e54e3c5c3a21676
Rameshkumar Krishnasamy <ramesh0505rk@gmail.com>
2026-07-15 08:44:21 +0530

    Updated env.

--- src/app/Environments/environment.ts
+++ src/app/Environments/environment.ts
@@ -2,6 +2,8 @@ export const environment = {
     production: false,
     // baseUrl: 'https://localhost:44322',
     // restApiUrl: 'https://localhost:44322/api'
-    baseUrl: 'http://192.168.0.5:44355',
-    restApiUrl: 'http://192.168.0.5:44355/api'
+    // baseUrl: 'http://192.168.0.5:44355',
+    // restApiUrl: 'http://192.168.0.5:44355/api'
+    baseUrl: 'http://192.168.0.4:44355',
+    restApiUrl: 'http://192.168.0.4:44355/api'
 }
